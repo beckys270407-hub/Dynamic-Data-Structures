6fb233de94245f4cc3fcd33929b1c2f58ae6fd90
Becky <beckys270407@gmail.com>
2026-04-18 20:02:07 -0400

    feat: add example usage for graph traversal and shortest path

--- graph_library/examples/main.cpp
+++ graph_library/examples/main.cpp
@@ -1,1 +1,27 @@
+#include "../include/Graph.hpp"
 
+using namespace std;
+
+int main() {
+    Graph g(5);
+
+    g.addEdge(0, 1, 2);
+    g.addEdge(0, 4, 1);
+    g.addEdge(1, 2, 3);
+    g.addEdge(1, 3, 2);
+    g.addEdge(3, 4, 4);
+
+    cout << "Graph:\n";
+    g.printGraph();
+
+    cout << "\nBFS: ";
+    g.BFS(0);
+
+    cout << "DFS: ";
+    g.DFS(0);
+
+    cout << "\nDijkstra:\n";
+    g.shortestPath(0);
+
+    return 0;
+}
