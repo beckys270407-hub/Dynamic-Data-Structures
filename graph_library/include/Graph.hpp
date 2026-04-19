407b738765bbdc117ba70513b4ab7a7bdc8a09f5
Becky <beckys270407@gmail.com>
2026-04-18 20:00:56 -0400

    feat: add Graph class definition and adjacency list structure

--- graph_library/include/Graph.hpp
+++ graph_library/include/Graph.hpp
@@ -1,1 +1,29 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
 
+#include <iostream>
+#include <list>
+#include <queue>
+#include <limits>
+
+using namespace std;
+
+class Graph {
+private:
+    int vertices;
+    list<pair<int, int>>* adjList; 
+
+public:
+    Graph(int v);
+    ~Graph();
+
+    void addEdge(int u, int v, int weight = 1);
+    void printGraph();
+
+    void BFS(int start);
+    void DFS(int start);
+
+    void shortestPath(int start); 
+};
+
+#endif
